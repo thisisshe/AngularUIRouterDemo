8affc2022674b8d49a73aef12e1e5871154c4043
unknown <caos@NAD2LCDV0000ITI.ES.AD.ADP.com>
2015-09-14 14:57:12 -0400

    Initial commit

---
+++ AngularUIRouterDemo/app/app.js
@@ -0,0 +1,33 @@
+﻿var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function ($stateProvider,$urlRouterProvider) {
+
+    $urlRouterProvider.when("", "/PageTab");
+
+    $stateProvider.state("PageTab",
+        {
+            url: "/PageTab",
+            templateUrl: "app/partials/PageTab.html" })
+
+        .state("PageTab.Page1",
+        { url: "/Page1", templateUrl: "app/partials/Page1.html" })
+
+        . state("PageTab.Page2",
+        { url: "/Page2", templateUrl: "app/partials/Page2.html" })
+
+        .state("PageTab.Page3",
+        { url: "/Page3", templateUrl: "app/partials/Page3.html" })
+
+        .state("PageTab.clients",
+        { url: "/clients", templateUrl: "app/partials/clients.html" })
+
+        .state("PageTab.services",
+        { url: "/services", templateUrl: "app/partials/services.html" })
+
+        .state("PageTab.Page1.skills",
+        {url:"/skills",templateUrl:'app/partials/skills.html'});
+
+
+
+
+});
